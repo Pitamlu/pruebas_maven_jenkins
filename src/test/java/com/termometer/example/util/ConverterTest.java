4bd33255796bd5829358dee40c602ad6d7147f96
Pitamlu <95584075+Pitamlu@users.noreply.github.com>
2022-06-19 18:37:32 +0200

    Update ConverterTest.java

--- src/test/java/com/termometer/example/util/ConverterTest.java
+++ src/test/java/com/termometer/example/util/ConverterTest.java
@@ -25,4 +25,13 @@ public class ConverterTest {
         assertEquals(expectedFarenheitResult,
                 converter.convertCelsiusFarenheit(celsiusInput), 0.02);
     }
+    
+    @Test
+    public void cambioDeCaF() {
+        double farenheitInput = 75;
+        double expectedCelsiusResult = 23.88;
+
+        assertEquals(expectedCelsiusResult,
+                converter.convertFarenheitCelsius(farenheitInput), 0.02);
+    }
 }
